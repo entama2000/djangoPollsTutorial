d2bf64c939cbb6da7a3cd09dc6a18e41182c963e
tendo7 <103039176+entama2000@users.noreply.github.com>
2023-05-07 17:30:29 -0400

    create template folder

--- mysite/pyapp/views.py
+++ mysite/pyapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index_template(request):
+    pyapp_data = {
+        'str' : 'Django',
+        'num' : range(15),
+    }
+    return render(request, 'index.html', pyapp_data)
