0013c3c2626f21dcfd8c1b70a543a75f84ae0077
tendo7 <103039176+entama2000@users.noreply.github.com>
2023-05-08 03:16:22 -0400

    database connect 2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'EDT'
+TIME_ZONE = 'US/Eastern'
 
 USE_I18N = True
 
