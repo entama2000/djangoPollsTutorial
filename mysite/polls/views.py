7d51c3d746c849e01cd7bee9ea0630a0409aa6ff
tendo7 <103039176+entama2000@users.noreply.github.com>
2023-05-10 16:42:06 -0400

    modify form

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,25 +1,29 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.http import Http404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 from .models import Choice, Question
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/result.html", {"question": question})
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "polls/result.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
